81f6dee90ae40f3f389ea3a6b160d615fddc9edc
thong-phn <mrthonglion@gmail.com>
2026-04-05 13:16:35 +0200

    update ihw save weight

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,15 @@
 cd .. 
 # python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.1-wCEL" --sparsity_weight 0.1 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
-python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.2-wCEL" --sparsity_weight 0.2 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
-python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.3-wCEL" --sparsity_weight 0.3 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
-python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.4-wCEL" --sparsity_weight 0.4 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
-python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.5-wCEL" --sparsity_weight 0.5 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
+# python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.2-wCEL" --sparsity_weight 0.2 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
+# python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.3-wCEL" --sparsity_weight 0.3 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
+# python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.4-wCEL" --sparsity_weight 0.4 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
+# python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.5-wCEL" --sparsity_weight 0.5 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
+
+# python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.6-wCEL" --sparsity_weight 0.6 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
+# python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.7-wCEL" --sparsity_weight 0.7 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
+# python wear_main_loso_channel_pruning.py --run_name "Channel-pruning0.8-wCEL" --sparsity_weight 0.8 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
+
+python wear_main_loso_channel_pruning.py --run_name "Channel-pruning1-wCEL" --sparsity_weight 1 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
+python wear_main_loso_channel_pruning.py --run_name "Channel-pruning2-wCEL" --sparsity_weight 2 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
+python wear_main_loso_channel_pruning.py --run_name "Channel-pruning4-wCEL" --sparsity_weight 4 --performance --stage1_model_path "/home/qphan/master-thesis/models/stage1/fft/wear_best_model_subject{subject}_val.pth"
 
